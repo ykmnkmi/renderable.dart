b550073b498f4e759467f621b0bd9358ea812115
Olzhas Suleimen <amajisenpai@gmail.com>
2020-03-27 10:45:24 +0600

    add simple parser

--- lib/src/parser.dart
+++ lib/src/parser.dart
@@ -1,17 +1,69 @@
 library parser;
 
-import 'template.dart';
+import 'ast.dart';
+import 'tokenizer.dart';
 
-class Parser {
-  const Parser();
+class Scanner {
+  Scanner(Iterable<Token> tokens) : iterator = tokens.iterator;
 
-  Template parse(String source) {
-    return null;
+  final Iterator<Token> iterator;
+
+  Token _peek;
+
+  Token next() {
+    if (_peek != null) {
+      Token token = _peek;
+      _peek = null;
+      return token;
+    }
+
+    return iterator.moveNext() ? iterator.current : null;
+  }
+
+  Token peek() {
+    return _peek = next();
   }
 }
 
-extension TemplateString on String {
-  Template parse() => const Parser().parse(this);
+class Parser {
+  static Parser _instance;
 
-  String render(Map<String, Object> context) => parse().render(context);
+  static Parser Function() _factory = () {
+    _instance = Parser._();
+    _factory = () => _instance;
+    return _instance;
+  };
+
+  factory Parser() {
+    return _factory();
+  }
+
+  Parser._();
+
+  List<Node> parse(String source) {
+    final Iterable<Token> tokens = Tokenizer().tokenize(source);
+    final Scanner scanner = Scanner(tokens);
+
+    final List<Node> nodes = <Node>[];
+    Token token;
+
+    while ((token = scanner.next()) != null) {
+      switch (token.type) {
+        case TokenType.text:
+          nodes.add(Text(token.lexeme));
+          break;
+        // case TokenType.commentStart:
+        //   if (scanner.next())
+        //   break;
+        default:
+          return null;
+      }
+    }
+
+    return nodes;
+  }
+
+  Text parseText(Token token) {
+    return Text(token.lexeme);
+  }
 }
