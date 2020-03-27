ab294d44274b921f0ae65246c1b601e48b9857e8
Olzhas Suleimen <amajisenpai@gmail.com>
2020-03-27 10:44:20 +0600

    add types

--- example/example.dart
+++ example/example.dart
@@ -2,8 +2,8 @@ import 'package:renderable/renderable.dart';
 
 void main(List<String> arguments) {
   final Map<String, Object> context = <String, Object>{'name': 'jhon'};
-  final Template renderable = r'hello {{ name }}'.parse();
+  final Template template = r'hello {{ name }}'.parse();
 
-  print(renderable.render(context));
+  print(template.render(context));
   print(r'hello {{ name }}'.render(context));
 }
