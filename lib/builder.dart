40883589070e325a732fa21397e501b701a9927e
Olzhas Suleimen <amajisenpai@gmail.com>
2020-12-26 15:04:27 +0600

    sync 1.0.0-dev+46

--- lib/builder.dart
+++ lib/builder.dart
@@ -14,24 +14,28 @@ import 'src/utils.dart';
 import 'src/visitor.dart';
 
 Builder htmlTemplateBuilder(BuilderOptions options) {
-  return HtmlTemplateBuilder(options.config);
+  return HtmlTemplateBuilder.fromConfig(options.config);
 }
 
 class HtmlTemplateBuilder extends Builder {
-  HtmlTemplateBuilder(Map<String, Object?> config) : formatter = DartFormatter() {
-    environment = Configuration(
-      commentBegin: config['comment_begin'] as String? ?? defaults.commentBegin,
-      commentEnd: config['comment_end'] as String? ?? defaults.commentEnd,
-      variableBegin: config['variable_begin'] as String? ?? defaults.variableBegin,
-      variableEnd: config['variable_end'] as String? ?? defaults.variableEnd,
-      blockBegin: config['block_begin'] as String? ?? defaults.blockBegin,
-      blockEnd: config['block_end'] as String? ?? defaults.blockEnd,
+  factory HtmlTemplateBuilder.fromConfig(Map<String, Object?> config) {
+    return HtmlTemplateBuilder(
+      Configuration(
+        commentBegin: config['comment_begin'] as String? ?? defaults.commentBegin,
+        commentEnd: config['comment_end'] as String? ?? defaults.commentEnd,
+        variableBegin: config['variable_begin'] as String? ?? defaults.variableBegin,
+        variableEnd: config['variable_end'] as String? ?? defaults.variableEnd,
+        blockBegin: config['block_begin'] as String? ?? defaults.blockBegin,
+        blockEnd: config['block_end'] as String? ?? defaults.blockEnd,
+      ),
     );
   }
 
-  final DartFormatter formatter;
+  HtmlTemplateBuilder(this.environment) : formatter = DartFormatter();
 
-  late Configuration environment;
+  final Configuration environment;
+
+  final DartFormatter formatter;
 
   @override
   Map<String, List<String>> get buildExtensions {
@@ -49,285 +53,137 @@ class HtmlTemplateBuilder extends Builder {
     name += 'Template';
 
     return buildStep.readAsString(buildStep.inputId).then<void>((source) {
-      final buffer = StringBuffer();
-      TemplateBuilder(name, buffer, Parser(environment).parse(source, path: inputId.path));
-      buildStep.writeAsString(buildStep.inputId.changeExtension('.html.dart'), formatter.format(buffer.toString()));
+      final nodes = Parser(environment).parse(source, path: inputId.path);
+      final generator = TemplateBuilder(name);
+      final code = formatter.format(generator.visit(nodes));
+      buildStep.writeAsString(buildStep.inputId.changeExtension('.html.dart'), code);
     });
   }
 }
 
-class TemplateBuilder extends Visitor<dynamic> {
-  TemplateBuilder(this.name, this.templateBuffer, List<Node> nodes)
-      : bodyBuffers = <StringBuffer>[],
-        imports = <String>{'import \'package:renderable/core.dart\';'},
-        names = <Name>{} {
-    pushBodyBuffer();
+class Frame {}
 
-    for (final node in nodes) {
-      if (node is Data || node is Expression) {
-        bodyBuffer.write('buffer.write(');
-        node.accept(this);
-        bodyBuffer.writeln(');');
-      } else {
-        throw 'implement statements: $node';
-      }
-    }
-
-    final template = popBodyBuffer().toString();
-
-    final imports = this.imports.toList();
-    imports.sort();
-    imports.forEach(templateBuffer.writeln);
-
-    templateBuffer.writeln();
-
-    templateBuffer.writeln('class $name implements Renderable {');
-    templateBuffer.writeln('const $name();');
-    templateBuffer.writeln();
-
-    templateBuffer.write('String render(');
-
-    if (names.isNotEmpty) {
-      templateBuffer.write('{');
-      templateBuffer.writeAll(names.map<String>((name) => '${name.type} ${name.name}'), ', ');
-      templateBuffer.write('}');
-    }
-
-    templateBuffer.write(') {');
-    templateBuffer.writeln('final buffer = StringBuffer();');
-    templateBuffer.write(template);
-    templateBuffer.writeln('return buffer.toString();');
-    templateBuffer.writeln('}');
-
-    templateBuffer.writeln('}');
-  }
+class TemplateBuilder extends Visitor<Frame, String> {
+  TemplateBuilder(this.name);
 
   final String name;
 
-  final StringBuffer templateBuffer;
-
-  final List<StringBuffer> bodyBuffers;
-
-  final Set<String> imports;
-
-  final Set<Name> names;
-
-  StringBuffer get bodyBuffer {
-    return bodyBuffers.last;
-  }
-
-  StringBuffer popBodyBuffer() {
-    return bodyBuffers.removeLast();
-  }
-
-  void pushBodyBuffer([String content = '']) {
-    bodyBuffers.add(StringBuffer(content));
+  String visit(List<Node> nodes) {
+    throw UnimplementedError('implement visit');
   }
 
   @override
-  void visitAttribute(Attribute node) {
-    node.expression.accept(this);
-    bodyBuffer.write('.');
-    bodyBuffer.write(node.attribute);
+  String visitAll(List<Node> nodes, Frame context) {
+    throw UnimplementedError('implement visitAll');
   }
 
   @override
-  void visitBinary(Binary node) {
-    throw 'implement visitBinary';
+  String visitAttribute(Attribute node, Frame context) {
+    throw UnimplementedError('implement visitAttribute');
   }
 
   @override
-  void visitCall(Call node) {
-    node.expression.accept(this);
-    writeCalling(node.arguments, node.keywordArguments, node.dArguments, node.dKeywordArguments);
+  String visitBinary(Binary node, Frame context) {
+    throw UnimplementedError('implement visitBinary');
   }
 
   @override
-  void visitCompare(Compare node) {
-    throw 'implement visitCompare';
+  String visitCall(Call node, Frame context) {
+    throw UnimplementedError('implement visitCall');
   }
 
   @override
-  void visitConcat(Concat node) {
-    pushBodyBuffer();
-
-    for (final expression in node.expressions) {
-      if (expression is Name) {
-        bodyBuffer.write(r'$');
-        expression.accept(this);
-      } else {
-        bodyBuffer.write(r'${');
-        expression.accept(this);
-        bodyBuffer.write('}');
-      }
-    }
-
-    final template = popBodyBuffer().toString();
-    var quote = '\'';
-
-    if (template.contains('\n') || template.contains('\r\n')) {
-      quote *= 3;
-    }
-
-    bodyBuffer.write('$quote$template$quote');
+  String visitCompare(Compare node, Frame context) {
+    throw UnimplementedError('implement visitCompare');
   }
 
   @override
-  void visitCondition(Condition node) {
-    throw 'implement visitCondition';
+  String visitConcat(Concat node, Frame context) {
+    throw UnimplementedError('implement visitConcat');
   }
 
   @override
-  void visitConstant(Constant<dynamic> node) {
-    bodyBuffer.write(represent(node.value));
+  String visitCondition(Condition node, Frame context) {
+    throw UnimplementedError('implement visitCondition');
   }
 
   @override
-  void visitData(Data node) {
-    bodyBuffer.write(wrapData(node.data));
+  String visitConstant(Constant<Object?> node, Frame context) {
+    throw UnimplementedError('implement visitConstant');
   }
 
   @override
-  void visitDictLiteral(DictLiteral node) {
-    writeCollection(node.pairs, '{', '}');
+  String visitData(Data node, Frame context) {
+    throw UnimplementedError('implement visitData');
   }
 
   @override
-  void visitFilter(Filter node) {
-    imports.add('import \'package:renderable/filters.dart\' as filters;');
-    bodyBuffer.write('filters.${node.name}');
-    writeCalling(<Expression>[node.expression, ...node.arguments], node.keywordArguments, node.dArguments, node.dKeywordArguments);
+  String visitDictLiteral(DictLiteral node, Frame context) {
+    throw UnimplementedError('implement visitDictLiteral');
   }
 
   @override
-  void visitIf(If node) {
-    throw 'implement visitIf';
+  String visitFilter(Filter node, Frame context) {
+    throw UnimplementedError('implement visitFilter');
   }
 
   @override
-  void visitItem(Item node) {
-    node.expression.accept(this);
-    bodyBuffer.write('[');
-    node.key.accept(this);
-    bodyBuffer.write('[');
+  String visitIf(If node, Frame context) {
+    throw UnimplementedError('implement visitIf');
   }
 
   @override
-  void visitKeyword(Keyword node) {
-    bodyBuffer.write(node.key);
-    bodyBuffer.write(': ');
-    node.value.accept(this);
+  String visitItem(Item node, Frame context) {
+    throw UnimplementedError('implement visitItem');
   }
 
   @override
-  void visitListLiteral(ListLiteral node) {
-    writeCollection(node.nodes);
+  String visitKeyword(Keyword node, Frame context) {
+    throw UnimplementedError('implement visitKeyword');
   }
 
   @override
-  void visitName(Name node) {
-    names.add(node);
-    bodyBuffer.write(node.name);
+  String visitListLiteral(ListLiteral node, Frame context) {
+    throw UnimplementedError('implement visitListLiteral');
   }
 
   @override
-  void visitOperand(Operand node) {
-    throw 'implement visitOperand';
+  String visitName(Name node, Frame context) {
+    throw UnimplementedError('implement visitName');
   }
 
   @override
-  void visitOutput(Output node) {
-    for (final node in node.nodes) {
-      if (node is Data || node is Expression) {
-        bodyBuffer.write('buffer.write(');
-        node.accept(this);
-        bodyBuffer.writeln(');');
-      } else {
-        throw 'implement statements: $node';
-      }
-    }
+  String visitOperand(Operand node, Frame context) {
+    throw UnimplementedError('implement visitOperand');
   }
 
   @override
-  void visitPair(Pair node) {
-    node.key.accept(this);
-    bodyBuffer.write(': ');
-    node.value.accept(this);
+  String visitOutput(Output node, Frame context) {
+    throw UnimplementedError('implement visitOutput');
   }
 
   @override
-  void visitSlice(Slice node) {
-    bodyBuffer.write('slice(');
-    node.expression.accept(this);
-    bodyBuffer.write(', ');
-    node.start.accept(this);
-
-    final stop = node.stop;
-
-    if (stop != null) {
-      bodyBuffer.write(', ');
-      stop.accept(this);
-    }
-
-    final step = node.step;
-
-    if (step != null) {
-      bodyBuffer.write(', ');
-      step.accept(this);
-    }
-
-    bodyBuffer.write(')');
+  String visitPair(Pair node, Frame context) {
+    throw UnimplementedError('implement visitPair');
   }
 
   @override
-  void visitTest(Test node) {
-    imports.add('import \'package:renderable/tests.dart\' as tests;');
-    bodyBuffer.write('tests.${node.name}');
-    writeCalling(<Expression>[node.expression, ...node.arguments], node.keywordArguments, node.dArguments, node.dKeywordArguments);
+  String visitSlice(Slice node, Frame context) {
+    throw UnimplementedError('implement visitSlice');
   }
 
   @override
-  void visitTupleLiteral(TupleLiteral node) {
-    writeCollection(node.nodes);
+  String visitTest(Test node, Frame context) {
+    throw UnimplementedError('implement visitTest');
   }
 
   @override
-  void visitUnary(Unary node) {
-    bodyBuffer.write(node.operator);
-    node.expression.accept(this);
+  String visitTupleLiteral(TupleLiteral node, Frame context) {
+    throw UnimplementedError('implement visitTupleLiteral');
   }
 
-  @protected
-  void writeCalling(List<Expression> arguments, List<Keyword> keywordArguments, [Expression? dArguments, Expression? dKeywordArguments]) {
-    bodyBuffer.write('(');
-
-    for (final param in arguments.cast<Node>().followedBy(keywordArguments.cast<Node>())) {
-      bodyBuffer.write(', ');
-      param.accept(this);
-    }
-
-    // TODO: add dynamic arguments
-
-    bodyBuffer.write(')');
-  }
-
-  @protected
-  void writeCollection(List<Node> nodes, [String open = '[', String close = ']']) {
-    bodyBuffer.write(open);
-
-    for (final node in nodes.take(nodes.length - 1)) {
-      node.accept(this);
-      bodyBuffer.write(', ');
-    }
-
-    nodes.last.accept(this);
-    bodyBuffer.write(close);
-  }
-
-  @protected
-  static String wrapData(String value) {
-    final quote = value.contains('\n') || value.contains('\r\n') ? "\'\'\'" : '\'';
-    value = value.replaceAll('\'', '\\\'');
-    return '$quote$value$quote';
+  @override
+  String visitUnary(Unary node, Frame context) {
+    throw UnimplementedError('implement visitUnary');
   }
 }
